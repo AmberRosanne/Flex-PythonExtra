66481e771fe3d99f7cd59854f14c5709f6da4f9a
Amber <AmberRosannee@outlook.com>
2020-10-22 11:01:40 +0200

    eerste 2 probeercodes

--- 01-Turtle/amber.py
+++ 01-Turtle/amber.py
@@ -1,13 +1,12 @@
 import turtle
+colors = ["red","purple","blue","green","orange","yellow"]
+turtle.speed(0)
+my_pen = turtle.Pen()
+turtle.bgcolor("black")
+for x in range(360):
+    my_pen.pencolor(colors[x%6])
+    my_pen.width(x/100+1)
+    my_pen.forward(x)
+    my_pen.left(59)
 
-for x in range(4):
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.left(45)
-    
 turtle.done()
