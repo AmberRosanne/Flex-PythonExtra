9f387204484ed723cc73e6ba24c3b43817d2e0a7
Amber <AmberRosannee@outlook.com>
2020-10-29 10:08:52 +0100

    nieuwe mappen gemaakt

--- 02-FilesFolder/huidige_map.py
+++ 02-FilesFolder/huidige_map.py
@@ -4,12 +4,16 @@ werkmap = os.getcwd()
 
 print("De huidige map is: " + werkmap)
 
+mapnaam = ""
+lengte_mapnaam = 0
 
-mapnaam = input("Welke naam wil je voor je map?")
-lengte_mapnaam = len(mapnaam)
+while lengte_mapnaam == 0:
+    mapnaam = input("Welke naam wil je voor je map?")
+    lengte_mapnaam = len(mapnaam)
 
-if lengte_mapnaam > 0:
-    os.mkdir(mapnaam)
-    print("De map " + mapnaam + " is gemaakt.")
-else:
-    print("Je hebt geen naam ingevoerd")
+    if lengte_mapnaam > 0:
+        os.mkdir(mapnaam)
+    else:
+        print("Je hebt geen naam ingevoerd")
+
+print("De map " + mapnaam + " is gemaakt")
